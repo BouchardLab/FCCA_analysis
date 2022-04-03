a6059cdb4100c3319728d926c3988f47fe448f9a
akumar01 <akumar.4db0@gmail.com>
2022-04-03 15:22:33 -0700

    sabes var args

--- submit_files/sabes_var_args.py
+++ submit_files/sabes_var_args.py
@@ -7,12 +7,12 @@ script_path = '/home/akumar/nse/neural_control/batch_analysis.py'
 desc = 'cross validate, disregeard selection, want to use cross validated ccm estimation as the criteria. Fit on M1 and S1, and marginal models'
 
 #data_path = os.environ['SCRATCH'] + '/sabes'
-data_path = '/media/akumar/Secondary/data/sabes'    
+data_path = '/mnt/Secondary/data/sabes'    
  
 # These are the data files that contain both M1 and S1 recordings.
-#data_files = glob.glob('%s/loco*' % data_path)
-#data_files.append('%s/indy_20160426_01.mat' % data_path)
-data_files = ['/lol%d' % i for i in range(15)]
+data_files = glob.glob('%s/loco*' % data_path)
+data_files.append('%s/indy_20160426_01.mat' % data_path)
+#data_files = ['/lol%d' % i for i in range(15)]
 
  # Load the data files and determine how many dof (neurons) there are in each recording
  # data_files = ['%s/%s' % (data_path, data_file) for data_file in data_files]
@@ -26,5 +26,6 @@ loader_args = [{'bin_width':50, 'filter_fn':'none', 'filter_kwargs':{}, 'boxcox'
                {'bin_width':50, 'filter_fn':'none', 'filter_kwargs':{}, 'boxcox':0.5, 'spike_threshold':100, 'region':'both'}]
 
 # Estimation score not important since we save all estimates and do selection subsequently
-task_args = [{'estimator': 'uoi', 'penalty': 'scad', 'self_regress':sr, 'continuous':False, 
-              'fit_type':'union_only', 'order':order, 'estimation_score':'BIC'} for order in [1, 2, 3, 4, 5] for sr in [True, False]]
+task_args = [{'estimator': 'uoi', 'penalty': 'scad', 'self_regress':sr, 'continuous':False, 'n_boots_sel':1, 'selection_frac':1., 'n_boots_est': 1, 'estimation_frac':0.9, 
+              'fit_type':'union_only', 'order':order, 'estimation_score':'null', 'fold_idx':idx} for order in [1, 2, 3, 4, 5] for sr in [True, False]
+              for idx in range(5)]

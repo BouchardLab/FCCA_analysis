9dd3488d8fb9cecf4e621677132efd85469a0bb8
akumar01 <akumar.4db0@gmail.com>
2023-01-11 13:12:11 -0800

    pushing nersc changes on cv_decoding_mpi for local debugging

--- analysis_scripts/cv_decoding_mpi.py
+++ analysis_scripts/cv_decoding_mpi.py
@@ -1,26 +1,11 @@
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.model_selection import KFold
-from dca_research.lqg import LQGComponentsAnalysis as LQGCA
-import matplotlib.pyplot as plt
 import sys
 import os
 import itertools
 import pickle
-import pandas as pd
 from tqdm import tqdm
-import scipy
 import time
-
-from loaders import load_cv
-from utils import apply_df_filters
-
-from sklearn.model_selection import StratifiedKFold
-from sklearn.linear_model import LogisticRegressionCV
-import h5py
-
-from mpi4py import MPI
-
+import pdb
 
 def gen_sbatch(savepath, T, reps, nseeds):
 
@@ -57,13 +42,32 @@ def gen_sbatch(savepath, T, reps, nseeds):
             t, _, seed = comb
             sb.write('srun -N 3 -n 130 shifter --entrypoint python cv_decoding_mpi.py %s %d %d &\n' % (savepath, t, seed))
 
+def manual_onehot_encode(y):
+
+    cvs = np.unique(y)
+    
+    yoh = np.zeros(y.size)
+    for i in range(y.size):
+        yoh[i] = list(cvs).index(y[i])
+
+    return yoh
+        
 if __name__ == '__main__':
+    from sklearn.model_selection import StratifiedKFold
+    from sklearn.linear_model import LogisticRegressionCV
+    import h5py
+
+    from mpi4py import MPI
+    from sklearn.preprocessing import OneHotEncoder    
+    from sklearn.decomposition import PCA
+    from sklearn.model_selection import KFold
+    from dca_research.lqg import LQGComponentsAnalysis as LQGCA
 
     comm = MPI.COMM_WORLD
 
-    if com.rank == 0:
+    if comm.rank == 0:
         #fname = '/mnt/Secondary/data/cv/EC2_hg.h5'
-        fname = '/mnt/sdb1/nc_data/EC2_hg.h5'
+        fname = os.environ['SCRATCH'] + '/cv/EC2_hg.h5'
         with h5py.File(fname, 'r') as f:
             X = np.squeeze(f['X'][:])
             y = f['y'][:]
@@ -76,6 +80,10 @@ if __name__ == '__main__':
     X = comm.bcast(X)
     y = comm.bcast(y)
 
+    #encoder = OneHotEncoder()
+    #yoh = encoder.fit_transform(np.reshape(y, (-1, 1)))
+    yoh = manual_onehot_encode(y)
+
     savepath = sys.argv[1]
     T = int(sys.argv[2])
     lqg_seed = int(sys.argv[3])
@@ -84,44 +92,47 @@ if __name__ == '__main__':
     trte = StratifiedKFold(n_splits=10, shuffle=True, random_state=20200831)
     train_test_idxs = list(trte.split(X, y))
     # Split dims and folds across ranks
-
-    tasks = itertools.product(dims, np.arange(10))
+    dims = np.concatenate([np.arange(1, 7), np.arange(7, 14, 2), [20, 25, 30]])
+    tasks = list(itertools.product(dims, np.arange(10)))
     tasks = np.array_split(tasks, comm.size)[comm.rank]
 
-
+    
     accuracy = np.zeros((len(tasks), 4))
 
     for ii, task in enumerate(tasks):
         t0 = time.time()
         d, fold_idx = task
+        print('rank %d working on (fold %d, dim %d)' % (comm.rank, fold_idx, d))
+
         train_idx = train_test_idxs[fold_idx][0]
         test_idx = train_test_idxs[fold_idx][1]
 
         Xtrain = X[train_idx]
-        ytrain = y[train_idx]
+        ytrain = yoh[train_idx]
         Xtest = X[test_idx]
-        ytest = y[test_idx]
+        ytest = yoh[test_idx]
         # dca_model = DCA(T=T, d=dims.max())
         # dca_model.estimate_data_statistics(Xtrain)
         lqg_model = LQGCA(T=T, d=d, rng_or_seed=lqg_seed)
         lqg_model.estimate_data_statistics(Xtrain)
-        print('rank %d working on (fold %d, dim %d)', % (comm.rank, fold_idx, d))
-
         pca_model = PCA(d)
         Xtrain_pca = pca_model.fit_transform(Xtrain.reshape(Xtrain.shape[0], -1))
         Xtest_pca = pca_model.transform(Xtest.reshape(Xtest.shape[0], -1))
+        print('fit PCA') 
         lr_model = LogisticRegressionCV(cv=5, multi_class='multinomial', max_iter=500)
         lr_model.fit(Xtrain_pca, ytrain)
-        
+        print('fit PCA LR')
         accuracy[ii, 0] = lr_model.score(Xtrain_pca, ytrain)
         accuracy[ii, 1] = lr_model.score(Xtest_pca, ytest)
         
         lqg_model.fit_projection(d=d, n_init=20)
-        
+        print('fit LQG')        
+
         Xtrain_dca = lqg_model.transform(Xtrain).reshape(Xtrain.shape[0], -1)
         Xtest_dca = lqg_model.transform(Xtest).reshape(Xtest.shape[0], -1)
         lr_model = LogisticRegressionCV(cv=5, multi_class='multinomial', max_iter=500)
         lr_model.fit(Xtrain_dca, ytrain)
+        print('fit LQG LR')
         accuracy[ii, 2] = lr_model.score(Xtrain_dca, ytrain)
         accuracy[ii, 3] = lr_model.score(Xtest_dca, ytest)
 
@@ -129,7 +140,7 @@ if __name__ == '__main__':
         pca_coef = pca_model.components_.T[:, 0:d]
         fca_scores = np.zeros(2)
         fca_scores[0] = -1 * np.max(lqg_model.scores)
-        fca_scores[1] = lqgmodel.score(coef=pca_model.components_.T[:, 0:d])
+        fca_scores[1] = lqg_model.score(coef=pca_model.components_.T[:, 0:d])
 
         # pca_scores = np.zeros(2)
         # pca_scores[0] = np.sum(pca_model.explained_variance_ratio_)
@@ -137,7 +148,7 @@ if __name__ == '__main__':
 
         # Save coefficients as well
 
-        print('rank %d completed in %f' % (time.time() - t0))
+        print('rank %d completed in %f' % (comm.rank, time.time() - t0))
         with open('%s/rank%d.pkl' % (savepath, comm.rank), 'ab') as f:
             f.write(pickle.dumps(task))
             f.write(pickle.dumps(accuracy))
